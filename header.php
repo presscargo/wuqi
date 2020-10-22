a79507911347854130bebb562d1dc98d188d7db5
David Sutoyo <sutoyod@yahoo.com>
2020-10-21 20:57:04 -0700

    Fix skip link

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'wuqi' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content-primary"><?php esc_html_e( 'Skip to content', 'wuqi' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="header-wrap">
